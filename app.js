4374bee84b04c42d03789ceb26e92ce4416c60a9
Ross Fenrick <rossfenrick@gmail.com>
2015-09-26 17:07:42 -0500

    Adding views

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('', function(req, res)  {
-	res.send('<h1>Hello</h1> Express');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res)  {
+	res.render('default', {title: 'Home'});
 });
 
 app.get('/me', function(req, res)  {
