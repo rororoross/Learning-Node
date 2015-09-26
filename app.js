c1afcb0ed0d51942016ff83d208e5deb3e756bbc
Ross Fenrick <rossfenrick@gmail.com>
2015-09-26 17:48:17 -0500

    Working with views

--- app.js
+++ app.js
@@ -3,23 +3,14 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.locals.pagetitle = 'Awesome Website';
+
 app.get('/', function(req, res)  {
 	res.render('default', {title: 'Home'});
 });
 
-app.get('/me', function(req, res)  {
-	res.send('@planetoftheweb');
-});
-
-app.get('/who/:name?', function(req, res)  {
-	var name = req.params.name;
-	res.send(name + ' was here');
-});
-
-app.get('/who/:name?/:title?', function(req, res)  {
-	var name = req.params.name;
-	var title = req.params.title;
-	res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+app.get('/about', function(req, res)  {
+	res.render('default', {title: 'About Us'});
 });
 
 app.get('*', function(req, res)  {
